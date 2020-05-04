d42bae56cbea6169dc0e461c6a08022185a2c2b6
Ike Ekwueme <cryptoekwu@gmail.com>
2020-05-04 18:59:50 -0500

    creeating inquirer inputs

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
-const util = require('util');
+const axios = require("./node_modules/axios");
+
+inquirer
+    .prompt([
+        {
+            type: "type",
+            name: "user",
+            message: "What is your username?"
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "What is the product name?"
+
+        },
+        {
+            type: "input",
+            name: "details",
+            message: "What's your project all about?"
+        },
+        {
+            type: "input",
+            name: "enviornment",
+            message: "How do you get the enviornment running?"
+        },
+        {
+            type: "input",
+            name: "toc",
+            message: "Create your table of contents!"
+        },
+        {
+            type: "input",
+            name: "output",
+            message: ""
+        },
+        {
+            type:,
+            name:,
+            message:
+        },
+        {
+            type: ,
+            name: ,
+            message:
+        },
+        {
+            type: ,
+            name: ,
+            message:
+        }
+    ])
 
 
 
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What would you like to title your markdown file?'
-    }
-]).catch( (answers) => {
 
-    var fileName = answers.str.toLowerCase().split('').join('') + '.md';
-
-    fs.writeFileSync(`${fileName}`)
-    
-    return console.log(err)
-
-});
 
 
 
