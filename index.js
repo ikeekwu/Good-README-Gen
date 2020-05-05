54df01d3d249c826ffc29f4ebdec6fdacb1a46d2
Ike Ekwueme <cryptoekwu@gmail.com>
2020-05-04 23:13:19 -0500

    MD creator

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const axios = require("./node_modules/axios");
 inquirer
     .prompt([
         {
-            type: "type",
+            type: "input",
             name: "user",
             message: "What is your username?"
         },
         {
             type: "input",
-            name: "title",
+            name: "name",
             message: "What is the product name?"
 
         },
@@ -22,35 +22,60 @@ inquirer
         },
         {
             type: "input",
-            name: "enviornment",
-            message: "How do you get the enviornment running?"
-        },
-        {
-            type: "input",
             name: "toc",
-            message: "Create your table of contents!"
+            message: "Create your table of contents!?"
         },
         {
             type: "input",
-            name: "output",
-            message: ""
+            name: "Installation",
+            message: "How to get this environment running?"
         },
         {
-            type:,
-            name:,
-            message:
+            type: "input",
+            name: "Usage",
+            message: "What is the utility of your product?"
         },
         {
-            type: ,
-            name: ,
-            message:
+            type: "input",
+            name: "collaborators",
+            message: "List people who you worked on this project with!"
         },
         {
-            type: ,
-            name: ,
-            message:
+            type: "input" ,
+            name: "Licensing",
+            message: "What license would you like to apply to this?"
         }
     ])
+        .then(function(response) {
+            axios.get(`https://api.github.com/users/${response.user}`)
+                .then(function(res) {
+                
+                    let md = `\n ![](${profilePic})
+
+                    \n ${res.data.avatar_url}\n
+                    \n ${res.data.email} \n
+
+                    \n # ${response.name} \n
+                    \n ${response.detail} \n
+                    \n # Table of contents: \n
+                    \n ${response.toc} \n
+                    \n # Install: \n
+                    \n ${response.Installation} \n
+                    \n # Usage: \n
+                    \n ${response.Usage} \n
+                    \n # Collaborators \n 
+                    \n ${response.collaborators} \n
+                    \n #Licenses: \n
+                    \n ${response.Licensing} \n
+                `
+
+                    fs.writeFile("README.md", md, function (err) {
+                        if (err) throw err;
+                        console.log("Markdown complete!")
+                    });
+            });
+        });
+
 
 
 
